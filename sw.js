9b58ab832dd8abf85930aa920ed3068ab690bb30
Prietto19 <inakiprietogea@gmail.com>
2026-03-18 12:32:06 +0100

    Update sw.js

---
+++ sw.js
@@ -0,0 +1,6 @@
+const CACHE = ‘7plus7-v9’;
+const BASE = ‘/tracker7-7/’;
+const ASSETS = [BASE, BASE+‘index.html’, BASE+‘manifest.json’, BASE+‘icon-192.png’, BASE+‘icon-512.png’];
+self.addEventListener(‘install’, e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); self.skipWaiting(); });
+self.addEventListener(‘activate’, e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))); self.clients.claim(); });
+self.addEventListener(‘fetch’, e => { e.respondWith(caches.match(e.request).then(cached => cached || fetch(e.request))); });
