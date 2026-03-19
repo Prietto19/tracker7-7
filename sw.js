b42bfef610094adecd5c19c979ca539f9fc67fed
Prietto19 <inakiprietogea@gmail.com>
2026-03-19 14:26:17 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,39 @@
-const CACHE = ‘7plus7-v9’;
-const BASE = ‘/tracker7-7/’;
-const ASSETS = [BASE, BASE+‘index.html’, BASE+‘manifest.json’, BASE+‘icon-192.png’, BASE+‘icon-512.png’];
-self.addEventListener(‘install’, e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); self.skipWaiting(); });
-self.addEventListener(‘activate’, e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))); self.clients.claim(); });
-self.addEventListener(‘fetch’, e => { e.respondWith(caches.match(e.request).then(cached => cached || fetch(e.request))); });
+var CACHE = ‘7plus7-v10’;
+var BASE = ‘/tracker7-7/’;
+
+self.addEventListener(‘install’, function(e) {
+e.waitUntil(
+caches.open(CACHE).then(function(c) {
+return c.addAll([
+BASE,
+BASE + ‘index.html’,
+BASE + ‘manifest.json’,
+BASE + ‘icon-192.png’,
+BASE + ‘icon-512.png’,
+‘https://unpkg.com/react@18.3.1/umd/react.production.min.js’,
+‘https://unpkg.com/react-dom@18.3.1/umd/react-dom.production.min.js’,
+‘https://unpkg.com/@babel/standalone@7.23.10/babel.min.js’,
+]).catch(function(){});
+})
+);
+self.skipWaiting();
+});
+
+self.addEventListener(‘activate’, function(e) {
+e.waitUntil(
+caches.keys().then(function(keys) {
+return Promise.all(keys.filter(function(k){ return k !== CACHE; }).map(function(k){ return caches.delete(k); }));
+})
+);
+self.clients.claim();
+});
+
+self.addEventListener(‘fetch’, function(e) {
+e.respondWith(
+caches.match(e.request).then(function(cached) {
+return cached || fetch(e.request).then(function(resp) {
+return resp;
+});
+})
+);
+});
